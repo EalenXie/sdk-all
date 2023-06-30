5367b86b567fc48afdde872ab4ad0be86148b887
EalenXie <ealenxie@qq.com>
2023-06-30 16:50:12 +0800

    major: 🚀 完成 OFFER 部分接口

--- allegro-sdk/src/main/java/io/github/ealenxie/allegro/offer/PendingChanges.java
+++ allegro-sdk/src/main/java/io/github/ealenxie/allegro/offer/PendingChanges.java
@@ -1,14 +1,14 @@
 package io.github.ealenxie.allegro.offer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author EalenXie created on 2023/6/30 15:15
  */
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class PendingChanges {
     /**
      * basePackage
--- allegro-sdk/src/main/java/io/github/ealenxie/allegro/offer/PromoModifyDetailResult.java
+++ allegro-sdk/src/main/java/io/github/ealenxie/allegro/offer/PromoModifyDetailResult.java
@@ -1,16 +1,16 @@
 package io.github.ealenxie.allegro.offer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 /**
  * @author EalenXie created on 2023/6/30 16:17
  */
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
 public class PromoModifyDetailResult {
     /**
      * tasks
