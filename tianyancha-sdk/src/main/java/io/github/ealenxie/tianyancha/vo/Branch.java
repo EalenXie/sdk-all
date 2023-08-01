7a373a272e5aed8f384c516e83fc2c6ff2662047
EalenXie <ealenxie@qq.com>
2023-08-01 09:32:57 +0800

    optimize: 🚀 TianYanChaClient 完善工商信息接口字段信息

--- tianyancha-sdk/src/main/java/io/github/ealenxie/tianyancha/vo/Branch.java
+++ tianyancha-sdk/src/main/java/io/github/ealenxie/tianyancha/vo/Branch.java
@@ -1,9 +1,79 @@
 package io.github.ealenxie.tianyancha.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author EalenXie created on 2023/7/31 13:45
  */
+@Getter
+@Setter
 public class Branch {
-    // TODO
+    /**
+     * 公司
+     */
+    @JsonProperty("id")
+    private int id;
+
+    /**
+     * 简称
+     */
+    @JsonProperty("alias")
+    private String alias;
+
+    /**
+     * 成立日期
+     */
+    @JsonProperty("estiblishTime")
+    private String estiblishTime;
+
+    /**
+     * 经营状态
+     */
+    @JsonProperty("regStatus")
+    private String regStatus;
+
+    /**
+     * 法人
+     */
+    @JsonProperty("legalPersonName")
+    private String legalPersonName;
+
+    /**
+     * 行业
+     */
+    @JsonProperty("category")
+    private String category;
+
+    /**
+     * 注册资本
+     */
+    @JsonProperty("regCapital")
+    private String regCapital;
+
+    /**
+     * 名称
+     */
+    @JsonProperty("name")
+    private String name;
+
+    /**
+     * 省份简称
+     */
+    @JsonProperty("base")
+    private String base;
+
+    /**
+     * 法人类型 1-人 2-公司
+     */
+    @JsonProperty("personType")
+    private int personType;
+
+    /**
+     * 企业logo
+     */
+    @JsonProperty("logo")
+    private String logo;
 
 }
