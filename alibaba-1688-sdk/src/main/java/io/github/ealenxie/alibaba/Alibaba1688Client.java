8a10eee72b43ca5effa776f1c8aa8bbfc2759d65
EalenXie <ealenxie@qq.com>
2023-09-04 15:06:23 +0800

    major: 🚀 Alibaba1688Client 逻辑优化

--- alibaba-1688-sdk/src/main/java/io/github/ealenxie/alibaba/Alibaba1688Client.java
+++ alibaba-1688-sdk/src/main/java/io/github/ealenxie/alibaba/Alibaba1688Client.java
@@ -2,7 +2,6 @@ package io.github.ealenxie.alibaba;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.ealenxie.alibaba.dto.RequireArgs;
 import org.apache.commons.codec.binary.Hex;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -32,18 +31,31 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 public abstract class Alibaba1688Client {
     private final RestOperations restOperations;
-
     private final ObjectMapper objectMapper;
+    private final String appKey;
+    private final String appSecret;
+    private String accessToken;
+
+    /**
+     * 设置 accessToken
+     */
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
     /**
      * 正式环境接口地址
      */
     public static final String HOST = "https://gw.open.1688.com/openapi/";
 
-    protected Alibaba1688Client() {
-        this(new ObjectMapper(), new RestTemplate());
+    protected Alibaba1688Client(String appKey, String appSecret, String accessToken) {
+        this(appKey, appSecret, accessToken, new ObjectMapper(), new RestTemplate());
     }
 
-    protected Alibaba1688Client(ObjectMapper objectMapper, RestOperations restOperations) {
+    protected Alibaba1688Client(String appKey, String appSecret, String accessToken, ObjectMapper objectMapper, RestOperations restOperations) {
+        this.appKey = appKey;
+        this.appSecret = appSecret;
+        this.accessToken = accessToken;
         this.restOperations = restOperations;
         this.objectMapper = objectMapper;
     }
@@ -60,14 +72,15 @@ public abstract class Alibaba1688Client {
      * 调用alibaba 1688接口
      *
      * @param serviceName   服务名
-     * @param requireArgs   系统级必填参数
      * @param dto           业务请求参数
      * @param typeReference 响应结果类型
      */
-    public <T> T postAlibaba(String serviceName, RequireArgs requireArgs, Object dto, ParameterizedTypeReference<T> typeReference) {
-        String urlNotHost = getUrlNotHost(serviceName, requireArgs.getAppKey());
+    public <T> T postAlibaba(String serviceName, Object dto, ParameterizedTypeReference<T> typeReference) {
+        String urlNotHost = getUrlNotHost(serviceName, appKey);
         URI uri = UriComponentsBuilder.fromHttpUrl(String.format("%s%s", HOST, urlNotHost)).build().encode().toUri();
-        return getRestOperations().exchange(uri, HttpMethod.POST, new HttpEntity<>(getArgs(urlNotHost, requireArgs, dto), getHeader()), typeReference).getBody();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        return getRestOperations().exchange(uri, HttpMethod.POST, new HttpEntity<>(getArgs(urlNotHost, dto), headers), typeReference).getBody();
     }
 
     /**
@@ -94,19 +107,26 @@ public abstract class Alibaba1688Client {
     /**
      * 构建请求参数
      *
-     * @param urlNotHost  不包含host的请求路径
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务请求参数
+     * @param urlNotHost 不包含host的请求路径
+     * @param dto        业务请求参数
      */
-    public MultiValueMap<String, Object> getArgs(String urlNotHost, RequireArgs requireArgs, Object dto) {
+    public MultiValueMap<String, Object> getArgs(String urlNotHost, Object dto) {
         SortedMap<String, Object> args = objectMapper.convertValue(dto, new TypeReference<SortedMap<String, Object>>() {
         });
         args.put("_aop_timestamp", String.valueOf(System.currentTimeMillis()));
-        if (!ObjectUtils.isEmpty(requireArgs.getAccessToken())) {
-            args.put("access_token", requireArgs.getAccessToken());
+        if (!ObjectUtils.isEmpty(accessToken)) {
+            args.put("access_token", accessToken);
         }
-        String signStr = getSignStr(urlNotHost, args);
-        String sign = getSign(signStr, requireArgs.getAppSecret());
+        // 获取阿里巴巴签名字符串
+        StringBuilder sb = new StringBuilder(urlNotHost);
+        Set<Map.Entry<String, Object>> entries = args.entrySet();
+        for (Map.Entry<String, Object> entry : entries) {
+            if (!entry.getKey().equals("_aop_signature")) {
+                sb.append(entry.getKey()).append(entry.getValue());
+            }
+        }
+        String signStr = sb.toString();
+        String sign = getSign(signStr, appSecret);
         args.put("_aop_signature", sign);
         MultiValueMap<String, Object> valueMap = new LinkedMultiValueMap<>();
         valueMap.setAll(args);
@@ -126,28 +146,4 @@ public abstract class Alibaba1688Client {
     }
 
 
-    /**
-     * 获取阿里巴巴签名字符串
-     *
-     * @param urlNotHost 不包含host的url路径
-     * @param urlParams  URL路径参数
-     * @return 签名字符串
-     */
-    public String getSignStr(String urlNotHost, SortedMap<String, Object> urlParams) {
-        StringBuilder signStr = new StringBuilder(urlNotHost);
-        Set<Map.Entry<String, Object>> entries = urlParams.entrySet();
-        for (Map.Entry<String, Object> entry : entries) {
-            if (!entry.getKey().equals("_aop_signature")) {
-                signStr.append(entry.getKey()).append(entry.getValue());
-            }
-        }
-        return signStr.toString();
-    }
-
-    public HttpHeaders getHeader() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        return headers;
-
-    }
 }
--- alibaba-1688-sdk/src/main/java/io/github/ealenxie/alibaba/CrossBorderERPClient.java
+++ alibaba-1688-sdk/src/main/java/io/github/ealenxie/alibaba/CrossBorderERPClient.java
@@ -17,175 +17,161 @@ import java.util.List;
  * @since 2022/12/15 15:42
  */
 public class CrossBorderERPClient extends Alibaba1688Client {
-    public CrossBorderERPClient() {
+    public CrossBorderERPClient(String appKey, String appSecret, String accessToken) {
+        super(appKey, appSecret, accessToken);
     }
 
-    public CrossBorderERPClient(ObjectMapper mapper, RestOperations restOperations) {
-        super(mapper, restOperations);
+    public CrossBorderERPClient(String appKey, String appSecret, String accessToken, ObjectMapper mapper, RestOperations restOperations) {
+        super(appKey, appSecret, accessToken, mapper, restOperations);
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.get.buyerView-1&aopApiCategory=trade_new">订单详情查看(买家视角)</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<TradeInfo> getTradeInfo(RequireArgs requireArgs, GetTradeInfo dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.get.buyerView-1", requireArgs, dto, new ParameterizedTypeReference<Resp<TradeInfo>>() {
+    public Resp<TradeInfo> getTradeInfo(GetTradeInfo dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.get.buyerView-1", dto, new ParameterizedTypeReference<Resp<TradeInfo>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.createCrossOrder-1">跨境订单创建</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<CreateCrossOrderResp> createCrossOrder(RequireArgs requireArgs, CreateCrossOrder dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.createCrossOrder-1", requireArgs, dto, new ParameterizedTypeReference<Resp<CreateCrossOrderResp>>() {
+    public Resp<CreateCrossOrderResp> createCrossOrder(CreateCrossOrder dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.createCrossOrder-1", dto, new ParameterizedTypeReference<Resp<CreateCrossOrderResp>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.createOrder.preview-1&aopApiCategory=trade_new">创建订单前预览数据接口</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public CreateOrderResp createOrder(RequireArgs requireArgs, CreateOrder dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.createOrder.preview-1", requireArgs, dto, new ParameterizedTypeReference<CreateOrderResp>() {
+    public CreateOrderResp createOrder(CreateOrder dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.createOrder.preview-1", dto, new ParameterizedTypeReference<CreateOrderResp>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.alipay.url.get-1">批量获取订单的支付链接</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public CrossBorderPayUrl alipayUrlGet(RequireArgs requireArgs, OrderIdListDTO dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.alipay.url.get-1", requireArgs, dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
+    public CrossBorderPayUrl alipayUrlGet(OrderIdListDTO dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.alipay.url.get-1", dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.product:alibaba.cross.productInfo-1">跨境场景获取商品详情</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public ProductInfoResp getProductInfo(RequireArgs requireArgs, GetProductInfo dto) {
-        return postAlibaba("com.alibaba.product:alibaba.cross.productInfo-1", requireArgs, dto, new ParameterizedTypeReference<ProductInfoResp>() {
+    public ProductInfoResp getProductInfo(GetProductInfo dto) {
+        return postAlibaba("com.alibaba.product:alibaba.cross.productInfo-1", dto, new ParameterizedTypeReference<ProductInfoResp>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.product.push:alibaba.cross.syncProductListPushed-1">跨境场景下将商品加入铺货列表</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<SyncProductResp> syncProductListPushed(RequireArgs requireArgs, SyncProductListPushed dto) {
-        return postAlibaba("com.alibaba.product.push:alibaba.cross.syncProductListPushed-1", requireArgs, dto, new ParameterizedTypeReference<Resp<SyncProductResp>>() {
+    public Resp<SyncProductResp> syncProductListPushed(SyncProductListPushed dto) {
+        return postAlibaba("com.alibaba.product.push:alibaba.cross.syncProductListPushed-1", dto, new ParameterizedTypeReference<Resp<SyncProductResp>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.OpQueryMarketingMixConfig-1&aopApiCategory=trade_new">查询卖家混批设置</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<OpMarketingMixConfig> opQueryMarketingMixConfig(RequireArgs requireArgs, OpQueryMarketingMixConfigDTO dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.OpQueryMarketingMixConfig-1", requireArgs, dto, new ParameterizedTypeReference<Resp<OpMarketingMixConfig>>() {
+    public Resp<OpMarketingMixConfig> opQueryMarketingMixConfig(OpQueryMarketingMixConfigDTO dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.OpQueryMarketingMixConfig-1", dto, new ParameterizedTypeReference<Resp<OpMarketingMixConfig>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.addresscode.parse-1&aopApiCategory=trade_new">根据地址解析地区码</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<ReceiveAddress> addressCodeParse(RequireArgs requireArgs, AddressCodeParse dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.addresscode.parse-1", requireArgs, dto, new ParameterizedTypeReference<Resp<ReceiveAddress>>() {
+    public Resp<ReceiveAddress> addressCodeParse(AddressCodeParse dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.addresscode.parse-1", dto, new ParameterizedTypeReference<Resp<ReceiveAddress>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.order.memoAdd-1&aopApiCategory=trade_new">修改订单备忘</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<Void> memoAdd(RequireArgs requireArgs, MemoAdd dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.order.memoAdd-1", requireArgs, dto, new ParameterizedTypeReference<Resp<Void>>() {
+    public Resp<Void> memoAdd(MemoAdd dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.order.memoAdd-1", dto, new ParameterizedTypeReference<Resp<Void>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.cancel-1&aopApiCategory=trade_new">取消交易</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<Void> tradeCancel(RequireArgs requireArgs, TradeCancel dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.cancel-1", requireArgs, dto, new ParameterizedTypeReference<Resp<Void>>() {
+    public Resp<Void> tradeCancel(TradeCancel dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.cancel-1", dto, new ParameterizedTypeReference<Resp<Void>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.logistics:alibaba.trade.getLogisticsInfos.buyerView-1&aopApiCategory=Logistics_NEW">获取交易订单的物流信息(买家视角)</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<List<LogisticsInfo>> getLogisticsInfos(RequireArgs requireArgs, GetLogisticsInfos dto) {
-        return postAlibaba("com.alibaba.logistics:alibaba.trade.getLogisticsInfos.buyerView-1", requireArgs, dto, new ParameterizedTypeReference<Resp<List<LogisticsInfo>>>() {
+    public Resp<List<LogisticsInfo>> getLogisticsInfos(GetLogisticsInfos dto) {
+        return postAlibaba("com.alibaba.logistics:alibaba.trade.getLogisticsInfos.buyerView-1", dto, new ParameterizedTypeReference<Resp<List<LogisticsInfo>>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.logistics:alibaba.trade.getLogisticsTraceInfo.buyerView-1&aopApiCategory=Logistics_NEW">获取交易订单的物流跟踪信息(买家视角)</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<List<LogisticsTraceInfo>> getLogisticsTraceInfo(RequireArgs requireArgs, GetLogisticsTraceInfo dto) {
-        return postAlibaba("com.alibaba.logistics:alibaba.trade.getLogisticsTraceInfo.buyerView-1", requireArgs, dto, new ParameterizedTypeReference<Resp<List<LogisticsTraceInfo>>>() {
+    public Resp<List<LogisticsTraceInfo>> getLogisticsTraceInfo(GetLogisticsTraceInfo dto) {
+        return postAlibaba("com.alibaba.logistics:alibaba.trade.getLogisticsTraceInfo.buyerView-1", dto, new ParameterizedTypeReference<Resp<List<LogisticsTraceInfo>>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.crossBorderPay.url.get-1">获取使用跨境宝支付的支付链接</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public CrossBorderPayUrl crossBorderPayUrlGet(RequireArgs requireArgs, OrderIdListDTO dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.crossBorderPay.url.get-1", requireArgs, dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
+    public CrossBorderPayUrl crossBorderPayUrlGet(OrderIdListDTO dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.crossBorderPay.url.get-1", dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.creditPay.url.get-1">获取使用诚e赊支付的支付链接</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public CrossBorderPayUrl creditPayUrlGet(RequireArgs requireArgs, OrderIdListDTO dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.creditPay.url.get-1", requireArgs, dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
+    public CrossBorderPayUrl creditPayUrlGet(OrderIdListDTO dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.creditPay.url.get-1", dto, new ParameterizedTypeReference<CrossBorderPayUrl>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.addFeedback-1&aopApiCategory=trade_new">买家补充订单留言接口</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<List<FeedbackResp>> addFeedback(RequireArgs requireArgs, AddFeedback dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.addFeedback-1", requireArgs, dto, new ParameterizedTypeReference<Resp<List<FeedbackResp>>>() {
+    public Resp<List<FeedbackResp>> addFeedback(AddFeedback dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.addFeedback-1", dto, new ParameterizedTypeReference<Resp<List<FeedbackResp>>>() {
         });
     }
 
@@ -193,66 +179,60 @@ public class CrossBorderERPClient extends Alibaba1688Client {
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.refund.OpQueryBatchRefundByOrderIdAndStatus-1&aopApiCategory=trade_new">查询退款单详情-根据订单ID（买家视角）</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<OpQueryBatchRefundResp> opQueryBatchRefundByOrderIdAndStatus(RequireArgs requireArgs, OpQueryBatchRefund dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.refund.OpQueryBatchRefundByOrderIdAndStatus-1", requireArgs, dto, new ParameterizedTypeReference<Resp<OpQueryBatchRefundResp>>() {
+    public Resp<OpQueryBatchRefundResp> opQueryBatchRefundByOrderIdAndStatus(OpQueryBatchRefund dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.refund.OpQueryBatchRefundByOrderIdAndStatus-1", dto, new ParameterizedTypeReference<Resp<OpQueryBatchRefundResp>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.account:alibaba.account.agent.basic-1&aopApiCategory=member">获取非授权用户的基本信息</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<SimpleAccountInfo> agentBasic(RequireArgs requireArgs, AgentBasic dto) {
-        return postAlibaba("com.alibaba.account:alibaba.account.agent.basic-1", requireArgs, dto, new ParameterizedTypeReference<Resp<SimpleAccountInfo>>() {
+    public Resp<SimpleAccountInfo> agentBasic(AgentBasic dto) {
+        return postAlibaba("com.alibaba.account:alibaba.account.agent.basic-1", dto, new ParameterizedTypeReference<Resp<SimpleAccountInfo>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=system.oauth2:subaccount.auth.add-1">批量添加子账号授权</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<AuthResultModel> authAdd(RequireArgs requireArgs, AddSubAccount dto) {
-        return postAlibaba("system.oauth2:subaccount.auth.add-1", requireArgs, dto, new ParameterizedTypeReference<Resp<AuthResultModel>>() {
+    public Resp<AuthResultModel> authAdd(AddSubAccount dto) {
+        return postAlibaba("system.oauth2:subaccount.auth.add-1", dto, new ParameterizedTypeReference<Resp<AuthResultModel>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.createRefund-1">创建退款退货申请</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<CreateRefundResp> createRefund(RequireArgs requireArgs, CreateRefund dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.createRefund-1", requireArgs, dto, new ParameterizedTypeReference<Resp<CreateRefundResp>>() {
+    public Resp<CreateRefundResp> createRefund(CreateRefund dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.createRefund-1", dto, new ParameterizedTypeReference<Resp<CreateRefundResp>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.getRefundReasonList-1">查询退款退货原因（用于创建退款退货）</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<RefundReasonResp> getRefundReasonList(RequireArgs requireArgs, GetRefundReasonList dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.getRefundReasonList-1", requireArgs, dto, new ParameterizedTypeReference<Resp<RefundReasonResp>>() {
+    public Resp<RefundReasonResp> getRefundReasonList(GetRefundReasonList dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.getRefundReasonList-1", dto, new ParameterizedTypeReference<Resp<RefundReasonResp>>() {
         });
     }
 
     /**
      * <a href="https://open.1688.com/api/apidocdetail.htm?id=com.alibaba.trade:alibaba.trade.uploadRefundVoucher-1">上传退款退货凭证</a>
      *
-     * @param requireArgs 系统级必填参数
-     * @param dto         业务参数
+     * @param dto 业务参数
      */
-    public Resp<UploadRefundVoucherResp> uploadRefundVoucher(RequireArgs requireArgs, UploadRefundVoucher dto) {
-        return postAlibaba("com.alibaba.trade:alibaba.trade.uploadRefundVoucher-1", requireArgs, dto, new ParameterizedTypeReference<Resp<UploadRefundVoucherResp>>() {
+    public Resp<UploadRefundVoucherResp> uploadRefundVoucher(UploadRefundVoucher dto) {
+        return postAlibaba("com.alibaba.trade:alibaba.trade.uploadRefundVoucher-1", dto, new ParameterizedTypeReference<Resp<UploadRefundVoucherResp>>() {
         });
     }
 
